03b303b0a13fa8683cab1070ebcf8ce9976e9456
JesbinJose <jesbinjose57@gmail.com>
2023-12-06 17:54:10 +0530

    added some diffrence in the some data

--- lib/constants/random_data.dart
+++ lib/constants/random_data.dart
@@ -2,6 +2,7 @@ final class SomeData {
   static Map<String, dynamic> map(String username) => {
         'username': username,
         'isAdmin': false,
-        'courses_taken': 0,
+        'isTutor': false,
+        'profile_pic':'https://www.google.com/imgres?imgurl=https%3A%2F%2F1fid.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fno-profile-picture-6-1024x1024.jpg&tbnid=miECn54OTpJJIM&vet=12ahUKEwju3bui0_qCAxUxTGwGHSZbCkMQMygiegUIARC_AQ..i&imgrefurl=https%3A%2F%2F1fid.com%2Fdefault-profile-pictures%2F&docid=UHJju9y8U-m9nM&w=1024&h=1024&q=no%20profile%20picture&client=opera-gx&ved=2ahUKEwju3bui0_qCAxUxTGwGHSZbCkMQMygiegUIARC_AQ',
       };
 }
