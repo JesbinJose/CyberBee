8bf1229d5bb4fc8eccbfa563a8f5b5a44ce8d030
JesbinJose <jesbinjose57@gmail.com>
2023-12-18 15:13:35 +0530

    show course screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:cyber_bee/constants/constants.dart';
-import 'package:cyber_bee/core/firebase/message/send_message.dart';
 import 'package:cyber_bee/core/firebase/options/firebase_options.dart';
 import 'package:cyber_bee/presentation/splash/spash.dart';
 import 'package:cyber_bee/push_notification.dart';
@@ -23,12 +22,6 @@ void main() async {
     appleProvider: AppleProvider.debug,
   );
   await PushNotifications.init();
-  SendPushNotification().sendMessage(
-    title: 'Jesbin',
-    message: "Hello how are you",
-    toUserId:
-        'dvfauoK1Q3yMkq88e4RYG8:APA91bGoFEHV9pCTvMI5PdAzxwvZVzIyGu8oM4PR3waWVz6dwxVEgl7SuR4emus7_Zf162UvQAIXs3AFziL8wAhjZyXBmcSqTNjmnmwZ5zOdOTmrCg2JpdXEOZ0GPcro9avnxgeemdFC',
-  );
   runApp(const MyApp());
 }
 
--- lib/presentation/user/single_course/screens/course_inside.dart
+++ lib/presentation/user/single_course/screens/course_inside.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cyber_bee/constants/constants.dart';
+import 'package:cyber_bee/core/firebase/courses/courses.dart';
 import 'package:flutter/material.dart';
 
 class CourseAllPartScreen extends StatelessWidget {
@@ -14,10 +16,46 @@ class CourseAllPartScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(course.id),
       ),
-      body:  Column(
-        children: [
-          Container(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: const BoxDecoration(
+                color: MyColors.iconPrimaryGreyColor,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(
+                    20,
+                  ),
+                ),
+              ),
+              child: StreamBuilder(
+                stream: GetAllCourseDetails.getAllLevels(course.id),
+                builder: (context, snapshot) {
+                  if (snapshot.data == null) return const SizedBox();
+                  return ListView.separated(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      final level = snapshot.data!.docs[index];
+                      return SizedBox(
+                        height: 50,
+                        child: Text(level['']),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(
+                      thickness: 2,
+                      color: Colors.black,
+                    ),
+                    itemCount: 4,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
