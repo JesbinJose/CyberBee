f0eb8cb96d8c4f3ef48b95bfba9e1579541785f9
JesbinJose <jesbinjose57@gmail.com>
2023-12-26 18:30:12 +0530

    navbar baground to transparent

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -11,73 +11,99 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.sizeOf(context);
     return Scaffold(
-      body: SizedBox(
-        height: size.height,
-        width: size.width,
-        child: ValueListenableBuilder<int>(
-          valueListenable: _currentIndex,
-          builder: (_, __, ___) {
-            return screens[_currentIndex.value];
-          },
-        ),
-      ),
-      bottomNavigationBar: Container(
-        margin: const EdgeInsets.fromLTRB(40, 0, 40, 40),
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        height: 60,
-        decoration: const BoxDecoration(
-          color: MyColors.secondarybackgroundColor,
-          borderRadius: BorderRadius.all(
-            Radius.circular(20.0),
-          ),
-        ),
-        width: size.width * 90,
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(20.0),
-          ),
-          child: Theme(
-            data: Theme.of(context).copyWith(
-              splashFactory: NoSplash.splashFactory,
-            ),
-            child: ValueListenableBuilder(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: size.height,
+            width: size.width,
+            child: ValueListenableBuilder<int>(
               valueListenable: _currentIndex,
               builder: (_, __, ___) {
-                return BottomNavigationBar(
-                  type: BottomNavigationBarType.fixed,
-                  selectedItemColor: MyColors.primaryRedColor,
-                  currentIndex: _currentIndex.value,
-                  onTap: (value) {
-                    _currentIndex.value = value;
-                  },
-                  showSelectedLabels: false,
-                  showUnselectedLabels: false,
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: FaIcon(
-                        FontAwesomeIcons.house,
-                        size: 20,
-                      ),
-                      label: 'Home',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: FaIcon(
-                        FontAwesomeIcons.book,
-                        size: 20,
-                      ),
-                      label: 'Course',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: FaIcon(
-                        FontAwesomeIcons.userTie,
-                        size: 20,
-                      ),
-                      label: 'profile',
-                    ),
-                  ],
-                );
+                return screens[_currentIndex.value];
               },
             ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: BottomNavBar(
+              size: size,
+              currentIndex: _currentIndex,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    super.key,
+    required this.size,
+    required ValueNotifier<int> currentIndex,
+  }) : _currentIndex = currentIndex;
+
+  final Size size;
+  final ValueNotifier<int> _currentIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(40, 0, 40, 40),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      height: 60,
+      decoration: const BoxDecoration(
+        color: MyColors.secondarybackgroundColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+      ),
+      width: size.width * 90,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            splashFactory: NoSplash.splashFactory,
+          ),
+          child: ValueListenableBuilder(
+            valueListenable: _currentIndex,
+            builder: (_, __, ___) {
+              return BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: MyColors.primaryRedColor,
+                currentIndex: _currentIndex.value,
+                onTap: (value) {
+                  _currentIndex.value = value;
+                },
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: FaIcon(
+                      FontAwesomeIcons.house,
+                      size: 20,
+                    ),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: FaIcon(
+                      FontAwesomeIcons.book,
+                      size: 20,
+                    ),
+                    label: 'Course',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: FaIcon(
+                      FontAwesomeIcons.userTie,
+                      size: 20,
+                    ),
+                    label: 'profile',
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
